5f34771fba49df302d1f5ab479b63fed7fd6e3af
WanderPuppy <93175680+WanderPuppy@users.noreply.github.com>
2021-11-03 19:49:20 +0900

    Update test.py

--- test.py
+++ test.py
@@ -1,88 +1,111 @@
- ​import​ ​time 
- ​import​ ​pyupbit 
- ​import​ ​datetime 
-  
- ​access​ ​=​ ​"YqSsA9cIqwPfMbVO0BcRxqeD0dVxDYxbACJVZxIG" 
- ​secret​ ​=​ ​"jUl1rn9L9xG8ro2uxxea41wDiDSlv3B3nz5iPeIB" 
-  
- ​def​ ​get_target_price​(​ticker​, ​k​): 
- ​    ​"""변동성 돌파 전략으로 매수 목표가 조회""" 
- ​    ​df​ ​=​ ​pyupbit​.​get_ohlcv​(​ticker​, ​interval​=​"day"​, ​count​=​2​) 
- ​    ​target_price​ ​=​ ​df​.​iloc​[​0​][​'close'​] ​+​ (​df​.​iloc​[​0​][​'high'​] ​-​ ​df​.​iloc​[​0​][​'low'​]) ​*​ ​k 
- ​    ​return​ ​target_price 
-  
- ​def​ ​get_start_time​(​ticker​): 
- ​    ​"""시작 시간 조회""" 
- ​    ​df​ ​=​ ​pyupbit​.​get_ohlcv​(​ticker​, ​interval​=​"day"​, ​count​=​1​) 
- ​    ​start_time​ ​=​ ​df​.​index​[​0​] 
- ​    ​return​ ​start_time 
-  
- ​def​ ​get_ma15​(​ticker​): 
- ​    ​"""15일 이동 평균선 조회""" 
- ​    ​df​ ​=​ ​pyupbit​.​get_ohlcv​(​ticker​, ​interval​=​"day"​, ​count​=​15​) 
- ​    ​ma15​ ​=​ ​df​[​'close'​].​rolling​(​15​).​mean​().​iloc​[​-​1​] 
- ​    ​return​ ​ma15 
-  
- ​def​ ​get_balance​(​ticker​): 
- ​    ​"""잔고 조회""" 
- ​    ​balances​ ​=​ ​upbit​.​get_balances​() 
- ​    ​for​ ​b​ ​in​ ​balances​: 
- ​        ​if​ ​b​[​'currency'​] ​==​ ​ticker​: 
- ​            ​if​ ​b​[​'balance'​] ​is​ ​not​ ​None​: 
- ​                ​return​ ​float​(​b​[​'balance'​]) 
- ​            ​else​: 
- ​                ​return​ ​0 
- ​    ​return​ ​0 
-  
- ​def​ ​get_current_price​(​ticker​): 
- ​    ​"""현재가 조회""" 
- ​    ​return​ ​pyupbit​.​get_orderbook​(​ticker​=​ticker​)[​"orderbook_units"​][​0​][​"ask_price"​] 
-  
- ​# predicted_close_price = 0 
- ​# def predict_price(ticker): 
- ​#     """Prophet으로 당일 종가 가격 예측""" 
- ​#     global predicted_close_price 
- ​#     df = pyupbit.get_ohlcv(ticker, interval="minute30") 
- ​#     df = df.reset_index() 
- ​#     df['ds'] = df['index'] 
- ​#     df['y'] = df['close'] 
- ​#     data = df[['ds','y']] 
- ​#     model = Prophet() 
- ​#     model.fit(data) 
- ​#     future = model.make_future_dataframe(periods=24, freq='H') 
- ​#     forecast = model.predict(future) 
- ​#     closeDf = forecast[forecast['ds'] == forecast.iloc[-1]['ds'].replace(hour=9)] 
- ​#     if len(closeDf) == 0: 
- ​#         closeDf = forecast[forecast['ds'] == data.iloc[-1]['ds'].replace(hour=9)] 
- ​#     closeValue = closeDf['yhat'].values[0] 
- ​#     predicted_close_price = closeValue 
- ​# predict_price("KRW-BTC") 
- ​# schedule.every().hour.do(lambda: predict_price("KRW-BTC")) 
-  
- ​# 로그인 
- ​upbit​ ​=​ ​pyupbit​.​Upbit​(​access​, ​secret​) 
- ​print​(​"autotrade start"​) 
-  
- ​# 자동매매 시작 
- ​while​ ​True​: 
- ​    ​try​: 
- ​        ​now​ ​=​ ​datetime​.​datetime​.​now​() 
- ​        ​start_time​ ​=​ ​get_start_time​(​"KRW-BTC"​) 
- ​        ​end_time​ ​=​ ​start_time​ ​+​ ​datetime​.​timedelta​(​days​=​1​) 
-  
- ​        ​if​ ​start_time​ ​<​ ​now​ ​<​ ​end_time​ ​-​ ​datetime​.​timedelta​(​seconds​=​10​): 
- ​            ​target_price​ ​=​ ​get_target_price​(​"KRW-BTC"​, ​0.32​) 
- ​            ​ma15​ ​=​ ​get_ma15​(​"KRW-BTC"​) 
- ​            ​current_price​ ​=​ ​get_current_price​(​"KRW-BTC"​) 
- ​            ​if​ ​target_price​ ​<​ ​current_price​ ​and​ ​ma15​ ​<​ ​current_price​: 
- ​                ​krw​ ​=​ ​get_balance​(​"KRW"​) 
- ​                ​if​ ​krw​ ​>​ ​5000​: 
- ​                    ​upbit​.​buy_market_order​(​"KRW-BTC"​, ​krw​*​0.9995​) 
- ​        ​else​: 
- ​            ​btc​ ​=​ ​get_balance​(​"BTC"​) 
- ​            ​if​ ​btc​ ​>​ ​0.00008​: 
- ​                ​upbit​.​sell_market_order​(​"KRW-BTC"​, ​btc​*​0.9995​) 
- ​        ​time​.​sleep​(​1​) 
- ​    ​except​ ​Exception​ ​as​ ​e​: 
- ​        ​print​(​e​) 
- ​        ​time​.​sleep​(​1​)
+import pyupbit
+import time
+import datetime 
+
+print("LOADING...")
+tickers = pyupbit.get_tickers(fiat="KRW")
+tickers_count = len(tickers)
+tickers_price = {}
+crypto_name = "KRW-BTC"
+
+def get_tickers(): #코인의 정보 수집
+    tickers = pyupbit.get_tickers(fiat="KRW")
+    tickers_cout = len(tickers) #코인의 개수 세기
+    return tickers
+
+def upload_tickers(tickers): # ticker의 이름과 가격 저장
+    for i in range(0, tickers_count-1):
+        tickers_price[tickers[i]] = tickers_tangent(tickers[i])
+        time.sleep(0.1)
+    print("Successful data upload")
+    return tickers_price
+    
+def tickers_tangent(ticker): # ticker의 5분 구간의 차이 구함 (5분후 가격 - 5분전 가격) / 5분전 가격 
+    df = pyupbit.get_ohlcv(ticker, count = 5, interval = "minute1")
+    delta = (df.iloc[-1]['close'] - df.iloc[0]['open']) / df.iloc[0]['open'] * 100
+    time.sleep(0.1)
+    return delta
+
+def get_balance(ticker): #잔고 조회
+    balances = upbit.get_balances()
+    for b in balances:
+        if b['currency'] == ticker:
+            if b['balance'] is not None:
+                return float(b['balance'])
+            else:
+                return 0
+    return 0
+
+def get_current_price(ticker): #현재가 조회
+    return pyupbit.get_orderbook(ticker=ticker)["orderbook_units"][0]["ask_price"]
+
+def get_start_time(ticker): #시작시간 조회
+    df = pyupbit.get_ohlcv(ticker, interval="day", count=1)
+    start_time = df.index[0]
+    return start_time
+
+def get_target_crypto(tickers): #상승률이 가장 큰 코인 조회
+    data = upload_tickers(tickers)
+    sdata = sorted(data.items(), key=lambda x: x[1], reverse=True) #sdata에 상승률이 높은 순대로 저장
+    most_crypto = sdata[:1] #상승률이 가장 높은 코인 저장
+    print("{0} : {1}".format(most_crypto[0][0], most_crypto[0][1]))
+    return most_crypto
+    
+# for i in range(0, tickers_count - 1):
+#     ticker = tickers[i]
+#     delta = tickers_tangent(ticker)
+#     # print(ticker, ':' , delta)
+#     time.sleep(0.05)
+
+# print(pyupbit.get_tickers(fiat="KRW"))
+# print(pyupbit.get_current_price("KRW-BTC"))
+
+
+# for i in range(0, tickers_count-1):
+#     print(tickers[i], pyupbit.get_current_price(tickers[i]))
+#     time.sleep(0.1)
+
+#로그인
+access = "YqSsA9cIqwPfMbVO0BcRxqeD0dVxDYxbACJVZxIG"
+secret = "jUl1rn9L9xG8ro2uxxea41wDiDSlv3B3nz5iPeIB"
+upbit = pyupbit.Upbit(access, secret)
+print("autotrade start")
+
+# target_crypto = get_target_crypto(tickers)
+# print(target_crypto)
+# print(target_crypto[0][0])
+# crypto_name = ""
+# print(crypto_name)
+# target_crypto_name = target_crypto[0][0]
+# if crypto_name != target_crypto_name: #다시 불러와도 상승률이 가장 큰 코인 변동있을 때
+#     crypto_name = target_crypto_name
+# print(crypto_name)
+
+
+while True:
+    try:
+        #상승률이 가장 큰 코인 갱신하기
+        target_crypto = get_target_crypto(tickers)
+        target_crypto_name = target_crypto[0][0]
+        if crypto_name != target_crypto_name: #상승률이 가장 큰 코인 변동있을 때
+            print("Change no.1 crypto {1}! ({0} -> {1})".format(crypto_name, target_crypto_name))
+            crypto_name = target_crypto_name
+            
+            #Sell Crypto
+            sell_crypto = get_balance(crypto_name)
+            upbit.sell_market_order(crypto_name, sell_crypto)
+            print("Successful Sell-trade")
+            
+            #Buy Crypto
+            krw = get_balance("KRW")
+            
+            if krw > 5000:
+                upbit.buy_market_order(crypto_name, krw*0.9995)
+                print("Successful Buy-trade")
+            
+
+        time.sleep(1)
+    except Exception as e:
+        print(e)
+        time.sleep(1)
+
